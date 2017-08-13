fba78c6f8f44e5325b2a75d5a15540dec054f232
kissrain <cjyang1123@gmail.com>
2017-08-13 23:04:05 +0800

    finish HammingDistance

--- src/com/rainbow/leetcode/HammingDistance.java
+++ src/com/rainbow/leetcode/HammingDistance.java
@@ -1,8 +1,33 @@
 package com.rainbow.leetcode;
 
+/**
+ * 关键点：
+ * - 异或运算
+ */
 public class HammingDistance {
     public static void main(String[] args) {
-        // write your code here
-        System.out.println(1);
+        int x = 1;
+        int y = 6;
+
+        // result is 3
+        System.out.println(new HammingDistance().hammingDistance1(x, y));
+        System.out.println(new HammingDistance().hammingDistance2(x, y));
+    }
+
+    public int hammingDistance1(int x, int y) {
+        int xorResult = x ^ y;
+        int distance = 0;
+        // get 1 count
+        while (xorResult != 0) {
+            if (xorResult % 2 != 0) {
+                distance++;
+            }
+            xorResult = xorResult >> 1;
+        }
+        return distance;
+    }
+
+    public int hammingDistance2(int x, int y) {
+        return Integer.bitCount(x ^ y);
     }
 }
