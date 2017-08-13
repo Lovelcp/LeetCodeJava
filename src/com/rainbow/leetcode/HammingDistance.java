ede2a49f99758cae1da386ee28b7540568ae0eea
kissrain <cjyang1123@gmail.com>
2017-08-13 22:43:41 +0800

    init

---
+++ src/com/rainbow/leetcode/HammingDistance.java
@@ -0,0 +1,8 @@
+package com.rainbow.leetcode;
+
+public class HammingDistance {
+    public static void main(String[] args) {
+        // write your code here
+        System.out.println(1);
+    }
+}
